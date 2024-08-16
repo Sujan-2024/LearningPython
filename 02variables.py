5eee7bab6ade57db5fff151e0397229dcfb7c5a9
Sujan Kurumbang <sujankurumbang2@gmail.com>
2024-08-16 21:03:49 +0545

    code Added

---
+++ 02variables.py
@@ -0,0 +1,9 @@
+Name = ("Sujan Kurumbang")
+Name = ("Murari Ghimire")
+print(f"My name is {Name}")
+
+
+num1 = 100
+num2 = 200
+sum = num1 + num2
+print(f"Total Sum is {sum}")
---
+++ 03calc.py
@@ -0,0 +1,14 @@
+num1 = 10
+num2 = 5
+sum = num1 + num2
+diff = num1 - num2
+div = num1 / num2
+intdiv = num1 // num2
+cube = num1 **3
+square = num1**2
+print(f"Total sum is {sum}")
+print(f"Total diff is {diff}")
+print(f"Total div is {div}")
+print(f"Total intdiv is {intdiv}")
+print(f"Total cube is {cube}")
+print(f"Total square is {square}")
